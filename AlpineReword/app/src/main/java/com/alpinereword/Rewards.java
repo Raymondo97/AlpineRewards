ce25363021c62794e26773b60f934cf01631f5e8
Yaswell De la Rosa <yaswell_de_la_rosa@claro.com.do>
2021-11-18 12:44:46 -0400

    Login and main activity
    Login and main activity

---
+++ AlpineReword/app/src/main/java/com/alpinereword/Rewards.java
@@ -0,0 +1,16 @@
+package com.alpinereword;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Rewards extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rewards);
+    }
+
+    //
+}
